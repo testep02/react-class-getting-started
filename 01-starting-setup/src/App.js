20aa13d861fe3c4260d98132c9faa6b6772cacaf
Travis Estep <travis.estep@live.com>
2021-04-22 02:05:20 -0400

    Made changes per class
    Made changes per the class instructions

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -31,7 +31,26 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}></ExpenseItem>
+      
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2021, 2, 28);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.67;
